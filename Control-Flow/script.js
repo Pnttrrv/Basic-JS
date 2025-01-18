ec08e3ed29baea2c9265e98d731df7162aefac47
pnttrrv <ndhkptr@gmail.com>
2025-01-18 15:05:43 +0700

    trying while and for

---
+++ Control-Flow/script.js
@@ -0,0 +1,8 @@
+let x = 1;
+
+alert('Mulai');
+while (x <= 10) {
+  console.log(`Hello World ${x}x`);
+  x++;
+}
+alert('Selesai');
---
+++ Latihan-angkot/angkot1.js
@@ -0,0 +1,11 @@
+let jmlAngkot = 10;
+let noAngkot = 1;
+
+// while (noAngkot <= jmlAngkot) {
+//   console.log(`Angkot no. ${noAngkot} beroperasi dengan baik.`);
+//   noAngkot++;
+// }
+
+for (noAngkot; noAngkot <= jmlAngkot; noAngkot++) {
+  console.log(`Angkot no. ${noAngkot} beroperasi dengan baik.`);
+}
