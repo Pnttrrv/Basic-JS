154148993e9c5232ab37e17f2252a3d59f4d0229
pnttrrv <ndhkptr@gmail.com>
2025-01-23 00:25:40 +0700

    more function learning

--- Function-Latihan/script.js
+++ Function-Latihan/script.js
@@ -9,24 +9,30 @@ function hitungVolume() {
   alert(volume);
 }
 
-function tambah() {
-  function nilai() {
-    let hasil = 0;
-    for (let i = 0; i < arguments.length; i++) {
-      hasil += arguments[i];
-    }
-    return hasil;
-  }
-  let add = nilai(1, 2, 3, 4);
-  alert(add);
-}
+// function hitungVolumeDuaKubus(a, b) {
+//   return a * a * a + b * b * b;
+// }
 
-function kali() {
-  function nilai(a, b) {
-    return a * b;
-  }
-  //   let a = parseInt(prompt('Masukkan nilai 1 : '));
-  //   let b = parseInt(prompt('Masukkan nilai 2 : '));
-  var hasil = nilai(tambah(1, 2), tambah(1, 4));
-  alert(hasil);
-}
+// alert(hitungVolumeDuaKubus(8, 3));
+
+// function tambah() {
+//   function nilai() {
+//     let hasil = 0;
+//     for (let i = 0; i < arguments.length; i++) {
+//       hasil += arguments[i];
+//     }
+//     return hasil;
+//   }
+//   let add = nilai(1, 2, 3, 4);
+//   alert(add);
+// }
+
+// function kali() {
+//   function nilai(a, b) {
+//     return a * b;
+//   }
+//   //   let a = parseInt(prompt('Masukkan nilai 1 : '));
+//   //   let b = parseInt(prompt('Masukkan nilai 2 : '));
+//   var hasil = nilai(tambah(1, 2), tambah(1, 4));
+//   alert(hasil);
+// }
