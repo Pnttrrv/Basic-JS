96f3baee533d09e4bd7edabdd9d24cd5cd931188
pnttrrv <ndhkptr@gmail.com>
2025-01-25 01:33:56 +0700

    adding and remove array

---
+++ Latihan-angkot/angkot7.js
@@ -0,0 +1,43 @@
+let penumpang = [];
+let tambahPenumpang = function (namaPenumpang, penumpang) {
+  if (penumpang.length == 0) {
+    penumpang.push(namaPenumpang);
+
+    return penumpang;
+  } else {
+    for (let i = 0; i < penumpang.length; i++) {
+      if (penumpang[i] == undefined) {
+        penumpang[i] = namaPenumpang;
+        return penumpang;
+      } else if (penumpang[i] == namaPenumpang) {
+        console.log('nama sudah ada');
+        return penumpang;
+      } else if (i == penumpang.length - 1) {
+        penumpang.push(namaPenumpang);
+        return penumpang;
+      }
+    }
+  }
+};
+
+let hapusPenumpang = function (namaPenumpang, penumpang) {
+  if (penumpang.length == 0) {
+    console.log('Angkot masih kosong!');
+    return penumpang;
+  } else {
+    for (let i = 0; i < penumpang.length; i++) {
+      if (namaPenumpang == penumpang[i]) {
+        penumpang[i] = undefined;
+        return penumpang;
+      } else if (
+        namaPenumpang !=
+        penumpang.find(function (e) {
+          return e == namaPenumpang;
+        })
+      ) {
+        console.log('Penumpang tidak ada di dalam angkot');
+        return penumpang;
+      }
+    }
+  }
+};
